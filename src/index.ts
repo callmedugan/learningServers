4676ada9152eb92c70b26db000242338d13b5c98
callmedugan <callmedugan@gmail.com>
2026-07-22 02:07:38 -0700

    reorg folders, created handler for readiness

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,20 @@
 import express from "express";
+import { Request, Response } from "express";
 
 const app = express();
-app.listen("8080")
+const PORT = 8080;
+
+//this is what will be sent to clients when they check to see if the server is up
+async function handlerReadiness(req: Request, res: Response): Promise<void>{
+    res.set("Content-Type", "text/plain; charset=utf-8");
+    res.send("OK");
+}
+
+app.get("/healthz", handlerReadiness);
+
+//serves the index.html etc. from the path given
+app.use("/app", express.static("./src/app"));
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
