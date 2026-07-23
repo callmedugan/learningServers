dcc52ea88cadb3cd962bbd99a2bddb3d35d0d7cf
callmedugan <callmedugan@gmail.com>
2026-07-23 00:57:39 -0700

    added /validate_chirp

--- src/index.ts
+++ src/index.ts
@@ -48,8 +48,39 @@ async function handlerWriteRequestCount(req: Request, res: Response){
 async function handlerResetRequestCount(req: Request, res: Response){
     res.set("Content-Type", "text/plain; charset=utf-8");
     config.fileserverHits = 0;
-    res.send("Hits: " + 0);
+    res.send("Hits: " + 0 + "\n");
     res.status(200);
+}
+
+//handler to validate that a chirp is 140 chars or less
+async function handlerValidateChirp(req: Request, res: Response) {
+    let body = ""
+
+    //add listener for new json data
+    req.on("data", (chunk) => {
+        body += chunk;
+    })
+
+    //when all data has arrived, try to parse and then check length
+    req.on("end", () => {
+        try {
+            const parsedBody = JSON.parse(body);
+            if(parsedBody?.body.length > 140){
+                res.status(400).send({
+                    "error": "Chirp is too long"
+                })
+            }
+            else{
+                res.status(200).send({
+                    "valid": true
+                })
+            }
+        } catch (err) {
+            res.status(400).send({
+                "error": "Something went wrong"
+            });
+        }
+    })
 }
 
 //main////////////////////////////////////////////////////////////////////////////////
@@ -61,10 +92,11 @@ async function main() {
     
     //admin
     app.use("/admin/metrics", handlerWriteRequestCount);
-    app.use("/admin/reset", handlerResetRequestCount)
+    app.post("/admin/reset", handlerResetRequestCount)
 
-    //for readiness
+    //api
     app.get("/api/healthz", handlerReadiness);
+    app.post("/api/validate_chirp", handlerValidateChirp);
 
     //serves the index.html etc. from the path given
     app.use("/app", express.static("./src/app"));
